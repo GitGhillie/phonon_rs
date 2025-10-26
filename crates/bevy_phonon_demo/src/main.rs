54362fa801815e3bc9c5227bf714f8e36e8543f4
GitGhillie <jillisnoordhoek@gmail.com>
2025-10-26 16:07:35 +0100

    Add some atmosphere and water

--- crates/bevy_phonon_demo/src/main.rs
+++ crates/bevy_phonon_demo/src/main.rs
@@ -1,18 +1,56 @@
-use bevy::prelude::*;
+use bevy::{
+    anti_alias::fxaa::Fxaa,
+    camera::Exposure,
+    light::{
+        AtmosphereEnvironmentMapLight, CascadeShadowConfigBuilder, DirectionalLightShadowMap,
+        light_consts::lux,
+    },
+    pbr::{Atmosphere, AtmosphereSettings, ScreenSpaceAmbientOcclusion, ScreenSpaceReflections},
+    post_process::bloom::Bloom,
+    prelude::*,
+    render::view::Hdr,
+};
 
 use bevy_editor_cam::prelude::*;
 use bevy_skein::SkeinPlugin;
 
+use crate::water::WaterPlugin;
+
+mod water;
+
 fn main() {
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Bevy Phonon demo".into(),
+                    ..default()
+                }),
+                ..default()
+            }),
             MeshPickingPlugin,
             DefaultEditorCamPlugins,
+            WaterPlugin,
             SkeinPlugin::default(),
         ))
+        .insert_resource(DirectionalLightShadowMap { size: 4096 })
+        .insert_resource(ClearColor(Color::Srgba(Srgba {
+            red: 0.02,
+            green: 0.02,
+            blue: 0.02,
+            alpha: 1.0,
+        })))
+        .insert_resource(AmbientLight::NONE)
         .add_systems(Startup, setup)
+        .add_systems(PostStartup, into_the_sky)
         .run();
+}
+
+/// Moves everything up so that the atmosphere looks it bit more atmospheric
+fn into_the_sky(mut tfs: Query<&mut Transform>) {
+    for mut tf in tfs.iter_mut() {
+        tf.translation.y = tf.translation.y + 5000.0;
+    }
 }
 
 /// set up a simple 3D scene
@@ -21,30 +59,56 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // circular base
-    commands.spawn((
-        Mesh3d(meshes.add(Circle::new(4.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
-        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
-    ));
     // cube
     commands.spawn((
         Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
         MeshMaterial3d(materials.add(Color::srgb_u8(124, 144, 255))),
         Transform::from_xyz(0.0, 0.5, 0.0),
     ));
-    // light
-    commands.spawn((
-        PointLight {
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_xyz(4.0, 8.0, 4.0),
-    ));
     // camera
     commands.spawn((
         EditorCam::default(),
         Camera3d::default(),
+        Hdr,
+        Msaa::Off,
+        ScreenSpaceAmbientOcclusion::default(),
+        ScreenSpaceReflections::default(),
+        Fxaa::default(),
+        // This is the component that enables atmospheric scattering for a camera
+        Atmosphere::EARTH,
+        // The scene is in units of 10km, so we need to scale up the
+        // aerial view lut distance and set the scene scale accordingly.
+        // Most usages of this feature will not need to adjust this.
+        AtmosphereSettings::default(),
+        // The directional light illuminance used in this scene
+        // (the one recommended for use with this feature) is
+        // quite bright, so raising the exposure compensation helps
+        // bring the scene to a nicer brightness range.
+        Exposure::SUNLIGHT,
+        // Tonemapper chosen just because it looked good with the scene, any
+        // tonemapper would be fine :)
+        //Tonemapping::AcesFitted,
+        // Bloom gives the sun a much more natural look.
+        Bloom::NATURAL,
+        // Enables the atmosphere to drive reflections and ambient lighting (IBL) for this view
+        AtmosphereEnvironmentMapLight::default(),
+        //VolumetricFog::default(),
         Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
+    // Sun
+    commands.spawn((
+        DirectionalLight {
+            shadows_enabled: true,
+            illuminance: lux::RAW_SUNLIGHT,
+            ..default()
+        },
+        Transform::from_xyz(1.0, 0.2, 0.3).looking_at(Vec3::ZERO, Vec3::Y),
+        CascadeShadowConfigBuilder::default().build(),
+        //VolumetricLight,
+    ));
+    // Add a fog volume.
+    // commands.spawn((
+    //     FogVolume::default(),
+    //     Transform::from_scale(Vec3::splat(35.0)),
+    // ));
 }
---
+++ crates/bevy_phonon_demo/src/water.rs
@@ -0,0 +1,109 @@
+//! Demonstrates screen space reflections in deferred rendering.
+
+use bevy::{
+    color::palettes::css::BLACK,
+    image::{
+        ImageAddressMode, ImageFilterMode, ImageLoaderSettings, ImageSampler,
+        ImageSamplerDescriptor,
+    },
+    math::vec4,
+    pbr::{DefaultOpaqueRendererMethod, ExtendedMaterial, MaterialExtension},
+    prelude::*,
+    render::render_resource::{AsBindGroup, ShaderType},
+    shader::ShaderRef,
+};
+
+/// This example uses a shader source file from the assets subdirectory
+const SHADER_ASSET_PATH: &str = "shaders/water_material.wgsl";
+
+/// A custom [`ExtendedMaterial`] that creates animated water ripples.
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct Water {
+    /// The normal map image.
+    ///
+    /// Note that, like all normal maps, this must not be loaded as sRGB.
+    #[texture(100)]
+    #[sampler(101)]
+    normals: Handle<Image>,
+
+    // Parameters to the water shader.
+    #[uniform(102)]
+    settings: WaterSettings,
+}
+
+/// Parameters to the water shader.
+#[derive(ShaderType, Debug, Clone)]
+struct WaterSettings {
+    /// How much to displace each octave each frame, in the u and v directions.
+    /// Two octaves are packed into each `vec4`.
+    octave_vectors: [Vec4; 2],
+    /// How wide the waves are in each octave.
+    octave_scales: Vec4,
+    /// How high the waves are in each octave.
+    octave_strengths: Vec4,
+}
+
+#[derive(Debug)]
+pub struct WaterPlugin;
+
+impl Plugin for WaterPlugin {
+    fn build(&self, app: &mut App) {
+        // Enable deferred rendering, which is necessary for screen-space
+        // reflections at this time. Disable multisampled antialiasing, as deferred
+        // rendering doesn't support that.
+        app.insert_resource(DefaultOpaqueRendererMethod::deferred())
+            .add_plugins(MaterialPlugin::<ExtendedMaterial<StandardMaterial, Water>>::default())
+            .add_systems(Startup, spawn_water);
+    }
+}
+
+// Spawns the water plane.
+fn spawn_water(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut water_materials: ResMut<Assets<ExtendedMaterial<StandardMaterial, Water>>>,
+) {
+    commands.spawn((
+        Mesh3d(meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(1.0)))),
+        MeshMaterial3d(water_materials.add(ExtendedMaterial {
+            base: StandardMaterial {
+                base_color: BLACK.into(),
+                perceptual_roughness: 0.0,
+                ..default()
+            },
+            extension: Water {
+                normals: asset_server.load_with_settings::<Image, ImageLoaderSettings>(
+                    "textures/water_normals.png",
+                    |settings| {
+                        settings.is_srgb = false;
+                        settings.sampler = ImageSampler::Descriptor(ImageSamplerDescriptor {
+                            address_mode_u: ImageAddressMode::Repeat,
+                            address_mode_v: ImageAddressMode::Repeat,
+                            mag_filter: ImageFilterMode::Linear,
+                            min_filter: ImageFilterMode::Linear,
+                            ..default()
+                        });
+                    },
+                ),
+                // These water settings are just random values to create some
+                // variety.
+                settings: WaterSettings {
+                    octave_vectors: [
+                        vec4(0.080, 0.059, 0.073, -0.062),
+                        vec4(0.153, 0.138, -0.149, -0.195),
+                    ],
+                    octave_scales: vec4(1.0, 2.1, 7.9, 14.9) * 5.0,
+                    octave_strengths: vec4(0.16, 0.18, 0.093, 0.044),
+                },
+            },
+        })),
+        Transform::from_scale(Vec3::splat(100.0)),
+    ));
+}
+
+impl MaterialExtension for Water {
+    fn deferred_fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
+    }
+}
