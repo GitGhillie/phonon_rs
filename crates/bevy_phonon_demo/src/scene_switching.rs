15d6d6ba3b6c23283a0bf431ae38d3d7ee6db7f3
GitGhillie <jillisnoordhoek@gmail.com>
2025-10-28 21:01:06 +0100

    wip scene state

--- crates/bevy_phonon_demo/src/scene_switching.rs
+++ crates/bevy_phonon_demo/src/scene_switching.rs
@@ -8,6 +8,16 @@ pub struct ScenePlugin;
 impl Plugin for ScenePlugin {
     fn build(&self, app: &mut App) {
         app.add_sub_state::<SceneSelection>()
-            .add_systems(OnEnter(SceneSelection::Intro), setup);
+            .add_systems(OnEnter(SceneSelection::Intro), setup)
+            .add_systems(Update, select_scene);
+    }
+}
+fn select_scene(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    state: Res<State<SceneSelection>>,
+    mut next_state: ResMut<NextState<SceneSelection>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::Space) {
+        *next_state = bevy::prelude::NextState::Pending(SceneSelection::Intro);
     }
 }
