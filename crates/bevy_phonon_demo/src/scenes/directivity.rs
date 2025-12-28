4e490ce7deafe22f48b90570e38dc896a26b74f4
GitGhillie <jillisnoordhoek@gmail.com>
2025-12-28 18:11:38 +0100

    Directivity controls

--- crates/bevy_phonon_demo/src/scenes/directivity.rs
+++ crates/bevy_phonon_demo/src/scenes/directivity.rs
@@ -3,7 +3,7 @@ use bevy_phonon::effects::spatializer::SpatializerNode;
 
 use crate::{
     DemoAssets,
-    scenes::{DemoScene, SceneSelection, TextAssets, text_shadow_component},
+    scenes::{DemoScene, SceneSelection, text_shadow_component},
 };
 use bevy_seedling::{
     prelude::{EffectsQuery, SampleEffects},
@@ -21,7 +21,7 @@ impl DemoScene for DirectivityDemo {
     fn update_systems(&self, app: &mut App, schedule: impl bevy::ecs::schedule::ScheduleLabel) {
         app.add_systems(
             schedule,
-            (rotate_cube, controls).run_if(in_state(SceneSelection::Directivity)),
+            (rotate_cube, controls, update_ui).run_if(in_state(SceneSelection::Directivity)),
         );
     }
 }
@@ -39,7 +39,7 @@ fn setup_scene(
         Name::from("Cube Directivity"),
         SamplePlayer::new(demo_assets.audio_sample.clone()).looping(),
         sample_effects![SpatializerNode::default()],
-        Mesh3d(meshes.add(Cuboid::new(1.0, 1.0, 1.0))),
+        Mesh3d(meshes.add(Cuboid::from_length(0.25))),
         MeshMaterial3d(materials.add(Color::srgb_u8(255, 144, 124))),
         Transform::from_xyz(0.0, 1.5, 0.0),
         DespawnOnExit(SceneSelection::Directivity),
@@ -47,11 +47,25 @@ fn setup_scene(
 }
 
 fn setup_ui(mut commands: Commands) {
-    let text = String::from_utf8(TextAssets::get("intro.md").unwrap().data.to_vec());
+    // todo: Example description
+    // let text = String::from_utf8(TextAssets::get("intro.md").unwrap().data.to_vec());
+    // commands.spawn((
+    //     DespawnOnExit(SceneSelection::Directivity),
+    //     Text::from(text.unwrap()),
+    //     text_shadow_component(),
+    //     Node {
+    //         position_type: PositionType::Absolute,
+    //         bottom: px(5),
+    //         left: px(15),
+    //         ..default()
+    //     },
+    // ));
+
     commands.spawn((
         DespawnOnExit(SceneSelection::Directivity),
-        Text::from(text.unwrap()),
+        Text::from("StatusText"),
         text_shadow_component(),
+        Name::from("StatusText"),
         Node {
             position_type: PositionType::Absolute,
             bottom: px(5),
@@ -78,23 +92,51 @@ fn controls(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     player: Single<&SampleEffects, With<SamplePlayer>>,
     mut effects: Query<&mut SpatializerNode>,
+    time: Res<Time<Real>>,
 ) -> Result {
     let mut effect = effects.get_effect_mut(&player)?;
+    let sensitivity = 0.5;
 
     if keyboard_input.just_pressed(KeyCode::Digit1) {
         effect.direct_effect_parameters.flags.directivity ^= true;
     }
-    if keyboard_input.just_pressed(KeyCode::Digit2) {
-        effect.simulator_settings.directivity.dipole_power = 1.0;
+    if keyboard_input.pressed(KeyCode::Digit2) {
+        effect.simulator_settings.directivity.dipole_power -= sensitivity * time.delta_secs();
     }
-    if keyboard_input.just_pressed(KeyCode::Digit3) {
-        effect.simulator_settings.directivity.dipole_power = 2.0;
+    if keyboard_input.pressed(KeyCode::Digit3) {
+        effect.simulator_settings.directivity.dipole_power += sensitivity * time.delta_secs();
     }
-    if keyboard_input.just_pressed(KeyCode::Digit4) {
-        effect.simulator_settings.directivity.dipole_weight = 0.0;
+    if keyboard_input.pressed(KeyCode::Digit4) {
+        effect.simulator_settings.directivity.dipole_weight -= sensitivity * time.delta_secs();
     }
-    if keyboard_input.just_pressed(KeyCode::Digit5) {
-        effect.simulator_settings.directivity.dipole_weight = 1.0;
+    if keyboard_input.pressed(KeyCode::Digit5) {
+        effect.simulator_settings.directivity.dipole_weight += sensitivity * time.delta_secs();
+    }
+
+    Ok(())
+}
+
+fn update_ui(
+    player: Single<&SampleEffects, With<SamplePlayer>>,
+    effects: Query<&SpatializerNode>,
+    mut text: Query<&mut Text, With<Name>>,
+) -> Result {
+    let effect = effects.get_effect(&player)?;
+    let directivity = effect.direct_effect_parameters.flags.directivity;
+    let power = effect.simulator_settings.directivity.dipole_power;
+    let weight = effect.simulator_settings.directivity.dipole_weight;
+
+    if let Ok(mut text) = text.single_mut() {
+        let strings = vec![
+            "Sound sources can emit sound with different intensities in different directions."
+                .to_string(),
+            "Press the following keys to affect the directivity:".to_string(),
+            format!("[1] - Directivity enabled: {directivity}"),
+            format!("[2/3] - Dipole weight: {weight}"),
+            format!("[4/5] - Dipole power: {power}"),
+        ];
+
+        text.0 = strings.join("\n");
     }
 
     Ok(())
