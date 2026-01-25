206725920c9087077f9b1012404f52fbf7a9a3fe
GitGhillie <jillisnoordhoek@gmail.com>
2026-01-25 09:42:29 +0100

    Start porting ambisonics/hrtf

--- crates/phonon/src/effects/mod.rs
+++ crates/phonon/src/effects/mod.rs
@@ -1,9 +1,11 @@
 //! Higher level audio effects which are controlled by parameters.
 //! In some cases these parameters are intended to be set by the simulators.
 
+pub mod ambisonics_binarual;
 pub mod binaural;
 pub mod direct;
 pub mod eq;
 pub mod gain;
+pub mod overlap_add_convolution;
 pub mod panning;
 pub mod reverb;
