a8cf38e46b04e3ae7a2065ce0c95bd4fb6037afb
GitGhillie <jillisnoordhoek@gmail.com>
2026-01-28 18:18:55 +0100

    WIP migrate to bevy 0.18

--- crates/bevy_phonon_demo/src/main.rs
+++ crates/bevy_phonon_demo/src/main.rs
@@ -7,9 +7,7 @@ use bevy::{
 };
 
 use avian3d::prelude::*;
-use bevy_ahoy::prelude::*;
 use bevy_asset_loader::prelude::*;
-use bevy_enhanced_input::prelude::*;
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 use bevy_phonon::{AudioListener, effects::spatializer::SpatializerNode, prelude::PhononPlugin};
 use bevy_seedling::{SeedlingPlugin, node::RegisterNode, sample::AudioSample};
@@ -48,8 +46,7 @@ fn main() {
             graphics::GraphicsPlugin,
             scene_switching::ScenePlugin,
             PhysicsPlugins::default(),
-            EnhancedInputPlugin,
-            AhoyPlugin::default(),
+            FreeCameraPlugin,
             WaterPlugin,
             SkeinPlugin::default(),
             EguiPlugin::default(),
@@ -60,7 +57,6 @@ fn main() {
         .add_plugins(bevy_phonon::debug::DebugPlugin) // Optional
         .add_plugins(LogDiagnosticsPlugin::default())
         .register_node::<SpatializerNode>()
-        .add_input_context::<PlayerInput>()
         .init_state::<AssetLoadingState>()
         .add_loading_state(
             LoadingState::new(AssetLoadingState::Loading)
@@ -68,63 +64,18 @@ fn main() {
                 .load_collection::<DemoAssets>(),
         )
         .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            (
-                capture_cursor.run_if(input_just_pressed(MouseButton::Left)),
-                release_cursor.run_if(input_just_pressed(KeyCode::Escape)),
-            ),
-        )
         .run();
 }
 
 /// Setup the common parts between the different scenes in this demo
 fn setup(mut commands: Commands) {
-    // Spawn the player
-    let player = commands
-        .spawn((
-            Name::from("Player"),
-            CharacterController::default(),
-            PlayerInput,
-            actions!(PlayerInput[
-                (
-                    Action::<Movement>::new(),
-                    // Normalize the input vector
-                    DeadZone::default(),
-                    Bindings::spawn((
-                        Cardinal::wasd_keys(),
-                        Axial::left_stick()
-                    ))
-                ),
-                (
-                    Action::<Jump>::new(),
-                    bindings![KeyCode::Space,  GamepadButton::South],
-                ),
-                (
-                    Action::<Crouch>::new(),
-                    bindings![KeyCode::ControlLeft, GamepadButton::LeftTrigger],
-                ),
-                (
-                    Action::<RotateCamera>::new(),
-                    // Tweak the mouse sensitivity here
-                    Scale::splat(0.05),
-                    Bindings::spawn((
-                        Spawn(Binding::mouse_motion()),
-                        Axial::right_stick()
-                    ))
-                ),
-            ]),
-            Transform::from_xyz(0.0, 20.0, 5.0),
-        ))
-        .id();
-
     // Spawn the player camera
     commands.spawn((
         Name::from("Camera"),
         Camera3d::default(),
         graphics::camera_components(),
         AudioListener,
-        CharacterControllerCameraOf::new(player),
+        FreeCamera::default(),
     ));
 
     // Spawn the floor collider
@@ -147,17 +98,4 @@ fn setup(mut commands: Commands) {
         SunDisk::EARTH,
         //VolumetricLight,
     ));
-}
-
-#[derive(Component, Default)]
-pub(crate) struct PlayerInput;
-
-fn capture_cursor(mut cursor: Single<&mut CursorOptions>) {
-    cursor.grab_mode = CursorGrabMode::Locked;
-    cursor.visible = true;
-}
-
-fn release_cursor(mut cursor: Single<&mut CursorOptions>) {
-    cursor.visible = true;
-    cursor.grab_mode = CursorGrabMode::None;
 }
